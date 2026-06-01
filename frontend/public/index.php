cd8d587d4725400ef6a2f007fa99a042b3063dca
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-01 22:44:16 +0800

    Update index.php

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -17,4 +17,7 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+var_dump($app->bound('view'));
+exit;
+
 $app->handleRequest(Request::capture());
