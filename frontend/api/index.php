93ef8452eea063d6ea0d4d813ee888c82dcc5900
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-03 23:49:58 +0800

    Update index.php

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -1,25 +1,19 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
 
 echo '<pre>';
 
-echo "Laravel version: ";
-echo \Illuminate\Foundation\Application::VERSION;
-echo PHP_EOL;
+echo "vendor exists: ";
+var_dump(is_dir(__DIR__.'/../vendor'));
 
-echo "packages.php: ";
-var_dump(file_exists(__DIR__.'/../bootstrap/cache/packages.php'));
+echo "package manifest class exists: ";
+var_dump(class_exists(\Illuminate\Foundation\PackageManifest::class));
 
-echo "services.php: ";
-var_dump(file_exists(__DIR__.'/../bootstrap/cache/services.php'));
+echo "installed.php exists: ";
+var_dump(file_exists(__DIR__.'/../vendor/composer/installed.php'));
 
-echo "Installed providers:" . PHP_EOL;
-
-$providers = require __DIR__.'/../bootstrap/providers.php';
-
-print_r($providers);
+echo "installed.json exists: ";
+var_dump(file_exists(__DIR__.'/../vendor/composer/installed.json'));
 
 exit;
