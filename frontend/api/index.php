6342e8cdcef9f29e514651be710e2fdfb45267e2
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-03 20:58:26 +0800

    Refactor index.php for Laravel initialization
    Updated index.php to include autoload and bootstrap files, and added version and provider checks.

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -1,3 +1,25 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+echo '<pre>';
+
+echo "Laravel version: ";
+echo \Illuminate\Foundation\Application::VERSION;
+echo PHP_EOL;
+
+echo "packages.php: ";
+var_dump(file_exists(__DIR__.'/../bootstrap/cache/packages.php'));
+
+echo "services.php: ";
+var_dump(file_exists(__DIR__.'/../bootstrap/cache/services.php'));
+
+echo "Installed providers:" . PHP_EOL;
+
+$providers = require __DIR__.'/../bootstrap/providers.php';
+
+print_r($providers);
+
+exit;
