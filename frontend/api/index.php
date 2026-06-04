b4dc8d12ed8fa9241ecf62ced5f00e1d476da177
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-04 23:44:43 +0800

    Remove debug checks from index.php
    Removed debug output for vendor directory and package manifest checks.

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -1,19 +1,3 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
-
-echo '<pre>';
-
-echo "vendor exists: ";
-var_dump(is_dir(__DIR__.'/../vendor'));
-
-echo "package manifest class exists: ";
-var_dump(class_exists(\Illuminate\Foundation\PackageManifest::class));
-
-echo "installed.php exists: ";
-var_dump(file_exists(__DIR__.'/../vendor/composer/installed.php'));
-
-echo "installed.json exists: ";
-var_dump(file_exists(__DIR__.'/../vendor/composer/installed.json'));
-
-exit;
