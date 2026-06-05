f0f74df3f8cbc4f98f494d9a93f8e027bb1b7f76
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-06 01:37:04 +0800

    Update index.php

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -3,4 +3,15 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__.'/../public/index.php';
+require __DIR__.'/../vendor/autoload.php';
+
+try {
+    $app = require __DIR__.'/../bootstrap/app.php';
+
+    echo "BOOTSTRAP OK";
+} catch (Throwable $e) {
+    echo "<pre>";
+    echo get_class($e) . "\n\n";
+    echo $e->getMessage() . "\n\n";
+    echo $e->getFile() . ":" . $e->getLine();
+}
