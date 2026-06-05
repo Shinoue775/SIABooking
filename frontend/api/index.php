e5876e6e548fb17ab8689669185f851db8587443
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-06 01:41:14 +0800

    Update index.php

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -9,10 +9,19 @@ $app = require __DIR__.'/../bootstrap/app.php';
 try {
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-    echo "KERNEL OK";
+    $request = Request::capture();
+
+    $response = $kernel->handle($request);
+
+    echo "REQUEST HANDLED OK<br>";
+    echo "STATUS: ".$response->getStatusCode();
+
 } catch (Throwable $e) {
+
     echo "<pre>";
-    echo get_class($e) . "\n\n";
-    echo $e->getMessage() . "\n\n";
-    echo $e->getFile() . ":" . $e->getLine();
+    echo "EXCEPTION:\n";
+    echo get_class($e)."\n\n";
+    echo $e->getMessage()."\n\n";
+    echo $e->getFile().":".$e->getLine()."\n\n";
+    echo $e->getTraceAsString();
 }
