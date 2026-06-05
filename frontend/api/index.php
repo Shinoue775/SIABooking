f0b48a649550a692af037b87804a618568eee585
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-06 02:00:57 +0800

    Update index.php

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -2,22 +2,35 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+$app = require __DIR__.'/../bootstrap/app.php';
+
 echo "<pre>";
 
-echo "packages.php: ";
-var_dump(file_exists(__DIR__.'/../bootstrap/cache/packages.php'));
+try {
 
-echo "services.php: ";
-var_dump(file_exists(__DIR__.'/../bootstrap/cache/services.php'));
+    $providers = require __DIR__.'/../bootstrap/cache/services.php';
 
-echo "\n";
+    foreach ($providers['providers'] as $provider) {
 
-if (file_exists(__DIR__.'/../bootstrap/cache/services.php')) {
-    echo substr(
-        file_get_contents(__DIR__.'/../bootstrap/cache/services.php'),
-        0,
-        500
-    );
+        echo "REGISTERING: $provider\n";
+
+        $instance = new $provider($app);
+
+        $instance->register();
+
+        echo "OK\n";
+    }
+
+} catch (\Throwable $e) {
+
+    echo "\nFAILED PROVIDER:\n";
+    echo $provider . "\n\n";
+
+    echo get_class($e) . "\n";
+    echo $e->getMessage() . "\n\n";
+
+    echo $e->getFile() . ':' . $e->getLine();
+
 }
 
 exit;
