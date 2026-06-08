06902df2235621a976d65a627ed41a261329ab7f
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-08 20:46:59 +0800

    Redirect API index to public index.php

--- frontend/api/index.php
+++ frontend/api/index.php
@@ -1,26 +1,3 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-echo "<pre>";
-
-$configFile = __DIR__.'/../config/app.php';
-
-echo "config file exists: ";
-var_dump(file_exists($configFile));
-
-echo "\n";
-
-$config = require $configFile;
-
-echo "config loaded: ";
-var_dump(is_array($config));
-
-echo "\n";
-
-echo "config keys:\n";
-print_r(array_keys($config));
-
-exit;
+require __DIR__.'/../public/index.php';
