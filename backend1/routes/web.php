3d000fca00fa689a2548654e9f41ddd6208ce0b7
Lorenxo <lingadlourence06@gmail.com>
2026-06-11 09:55:22 +0800

    debug:backendAPI

--- backend1/routes/web.php
+++ backend1/routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn () => response()->json([
+Route::get('/', static fn() => response()->json([
     'name' => 'SIABooking backend1 API',
     'framework' => 'Laravel',
     'status' => 'ok',
 ]));
+
+Route::get('/debug', function () {
+    return response()->json([
+        'debug' => 'working',
+    ]);
+});
