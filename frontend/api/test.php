86fc9907a483898388e8ba7a77bced56744fcad1
Shinoue775 <154672603+Shinoue775@users.noreply.github.com>
2026-06-11 18:55:47 +0800

    Update test.php

--- frontend/api/test.php
+++ frontend/api/test.php
@@ -4,15 +4,3 @@ echo json_encode([
     'success' => true,
     'php' => PHP_VERSION,
 ]);
-
-"routes": [
-  {
-    "src": "/test",
-    "dest": "/api/test.php"
-  },
-  {
-    "src": "/(.*)",
-    "dest": "/api/index.php"
-  }
-]
-
